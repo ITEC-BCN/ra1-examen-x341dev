9688a5ceb1e2edda55d2d3c5e120dab8f9169861
x341dev <admin@x341.dev>
2026-01-30 08:38:38 +0100

    fix: android plugin dependency missing

--- app/src/main/java/com/example/adivinaapp/MainActivity.kt
+++ app/src/main/java/com/example/adivinaapp/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.adivinaapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Text("Hola :3")
         }
     }
 }
